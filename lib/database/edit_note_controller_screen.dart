b3762a083fe42eb2408dbc0396555b6118682282
Bassem <bassemgamalosman12@gmail.com>
2023-01-11 18:42:46 +0200

    fix edit bug

--- lib/database/edit_note_controller_screen.dart
+++ lib/database/edit_note_controller_screen.dart
@@ -4,23 +4,19 @@ import 'package:flutter/material.dart';
 class EditNoteController {
   SqlDb sqlDb = SqlDb();
 
-  Future<EditNoteController> edit(
+  Future<void> edit(
       {required String note, required String title, required int id}) async {
     int? response = await sqlDb.updateData('''
                         UPDATE notes SET
                         note = "$note" ,
-                        title =  "$title" ,
-                        WHERE id = "$id",
-
+                        title = "$title"
+                        WHERE id = "$id"
                         ''');
-
     if (response != 0) {
       print(response);
       print('Edit success');
     } else {
       print('Failed to edit the note!');
     }
-    return EditNoteController();
-    //return edit(note: note, title: title, id: id);
   }
 }
